cc333fcb6108aab60f2bb00c99f5cf4bf3658887
slowcookerenthusiasts <jgbeninger@mta.ca>
2016-10-27 11:24:42 -0300

    Skeleton for view

--- Tick Attack V1/src/GameView.java
+++ Tick Attack V1/src/GameView.java
@@ -4,6 +4,18 @@ public class GameView {
 	public void removeFromView() {
 		
 	}
+	
+	public void showHealth() {
+		
+	}
+	
+	public void showStreetCred() {
+		
+	}
+	
+	public void showInventory() {
+		
+	}
 
 	//show health,streetcred, inventory, etc.
 }
